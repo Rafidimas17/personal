fdf35f11a4b55643b1f8d41817d1ec94c48f3393
Rafidimas17 <rafidimas1706@gmail.com>
2023-11-30 14:12:32 +0700

    update app

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
-// import Topbar from "./Components/topbar/Topbar";
-// import "./App.scss";
 import React, { useState } from "react";
-// import Menu from "./Components/menu/menu";
-// import Intro from "./Components/Intro/intro";
-// import Portofolio from "./Components/Portofolio/Portofolio";
-// import Skills from "./Components/Skills/Skills";
-// import Contact from "./Components/Contact/contact";
-
+import Topbar from "./Components/topbar/Topbar";
+import "./App.scss";
+import Menu from "./Components/menu/menu";
+import Intro from "./Components/Intro/intro";
+import Portofolio from "./Components/Portofolio/Portofolio";
+import Skills from "./Components/Skills/Skills";
+import Contact from "./Components/Contact/contact";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
-    <h5>Wesbite sedang dalam proses perbaikan...</h5>
-      // <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      // <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      // <Intro />
-      // <div className="container">
-      //   <Skills />
-      //   <Portofolio />
-      //   <Contact />
-      // </div>
-      // <div class="card" style={{ border: "none" }}>
-      //   <div class="card-body">
-      //     <p
-      //       style={{ fontFamily: "Poppins", fontSize: 15 }}
-      //       className="text-center"
-      //     >
-      //       Create by rafidimas &#07; 2023
-      //     </p>
-      //     <footer class="footer"></footer>
-      //   </div>
-      // </div>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <div className="container">
+        <Intro />
+        <Skills />
+        <Portofolio />
+        <Contact />
+      </div>
+      <div class="card" style={{ border: "none" }}>
+        <div class="card-body">
+          <p
+            style={{ fontFamily: "Poppins", fontSize: 15 }}
+            className="text-center">
+            Create by rafidimas 2023
+          </p>
+          <footer class="footer"></footer>
+        </div>
+      </div>
     </div>
   );
 }
